fd52c19bef526a69c19076378a622f295faf86ed
josermoran <00135321@uca.edu.sv>
2022-11-08 12:08:08 -0600

    laboratorio8

---
+++ trabajo final lab.cpp
@@ -0,0 +1,15 @@
+1.¿Que es ser autodidacta?
+ Es la capacidad de una persona,para poder llenarse y aprender de mucha informacion por si sola, ya sea cosas de estudio,profesional,etc.
+
+2.Investigar que son matrices(definicion,sintaxis,etc).
+
+Es una estructura de datos,o un espacio de memoria que permite almacenar una colección de elementos, todos son del mismo tipo. La diferncia de los arreglos y las
+matrices consiste en que los elementos no están organizados linealmente, sino que la organización de ellos es bidimensional, es decir, en filas y columnas.
+
+Su sintaxis es:El TIPO DE DATO NOMBRE [TAMAÑO 1][TAMAÑO 2]
+Donde el tamaño de la matriz, es el número de filas multiplicado por el número de columnas, empezando desde cero.
+
+3.Listar 3 fuentes de donde aprender,(canales de youtube,foros,paginas,etc.).
+1. https://www.youtube.com/watch?v=XYdOLf_iE7M
+2.https://www.youtube.com/watch?v=v3ENcQpoA5A
+3.https://www.youtube.com/watch?v=p09i_hoFdd0
